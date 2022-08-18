9333f001b52b19e4145df196cc89b40073ec05c6
James Wu <ichangjwu@outlook.com>
2022-08-19 00:44:41 +0900

    Add tests and implementation for Target

--- src/components/Target.js
+++ src/components/Target.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Target() {
-  return <Wrapper role="figure"></Wrapper>;
+export default function Target({ visible=false, onClick1, onClick2 }) {
+
+  const handleClick = () => {
+    onClick1();
+    onClick2();
+  }
+
+  return <Wrapper visible={visible} role="figure" onClick={handleClick}></Wrapper>;
 }
 
 const Wrapper = styled.div`
-  display: none;
+  display: ${props => props.visible ? "initial" : "none"};
   width: 50px;
   height: 50px;
   border: 3px solid red;
--- src/components/Target.test.js
+++ src/components/Target.test.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { render, screen } from '@testing-library/react';
-import Target from './Target';
+import { render, screen } from "@testing-library/react";
+import Target from "./Target";
+import userEvent from "@testing-library/user-event";
 
-
-describe('Target', () => {
-  it('renders a hidden empty circle with a red border', () => {
+describe("Target", () => {
+  it("renders a hidden empty circle with a red border", () => {
     render(<Target />);
-    const target = screen.getByRole('figure', { hidden: true });
+    const target = screen.getByRole("figure", { hidden: true });
     const style = window.getComputedStyle(target);
 
     expect(target).not.toBeVisible();
-    expect(style.borderColor).toBe('red');
-    expect(style.backgroundColor).toBe('transparent');
-  })
+    expect(style.borderColor).toBe("red");
+    expect(style.backgroundColor).toBe("transparent");
+  });
+
+  it("renders a visible circle determined by props", () => {
+    render(<Target visible={true} />);
+    const target = screen.getByRole("figure");
+
+    expect(target).toBeVisible();
+  });
+
+  it("calls moveTarget and moveDropdownMenu when the target is clicked", () => {
+    const moveTargetMock = jest.fn();
+    const moveDropdownMenuMock = jest.fn();
+    render(
+      <Target
+        visible={true}
+        onClick1={moveTargetMock}
+        onClick2={moveDropdownMenuMock}
+      />
+    );
+    const target = screen.getByRole("figure");
+
+    userEvent.click(target);
+
+    expect(moveTargetMock).toHaveBeenCalled();
+    expect(moveDropdownMenuMock).toHaveBeenCalled();
+  });
 });
