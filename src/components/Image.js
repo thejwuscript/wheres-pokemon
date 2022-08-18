8819ee9876e224feb2286ee89390c9ed3cac6f24
James Wu <ichangjwu@outlook.com>
2022-08-18 21:16:28 +0900

    Write test and implementation for Image component

---
+++ src/components/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PokemonImage from '../images/pokemon.webp';
+
+export default function Image( {onClick1, onClick2} ) {
+
+  const handleClick = (e) => {
+    onClick1();
+    onClick2();
+  };
+
+  return (
+    <img src={PokemonImage} alt="pokemon" onClick={handleClick} />
+  )
+};
---
+++ src/components/Image.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import Image from './Image';
+import userEvent from '@testing-library/user-event';
+
+describe('Image', () => {
+  it('renders an image with the expected source', () => {
+    render(<Image />);
+    const image = screen.getByRole('img');
+    expect(image).toBeVisible();
+    expect(image).toHaveAttribute('src', 'pokemon.webp')
+  });
+
+  it('calls moveTarget when the image is clicked', () => {
+    const moveTargetMock = jest.fn();
+    const moveDropdownMenuMock = jest.fn();
+    render(<Image onClick1={moveTargetMock} onClick2={moveDropdownMenuMock} />);
+    const image = screen.getByRole('img');
+
+    userEvent.click(image);
+    expect(moveTargetMock).toHaveBeenCalled();
+  });
+
+  it('calls moveDropdownMenu when the image is clicked', () => {
+    const moveDropdownMenuMock = jest.fn();
+    const moveTargetMock = jest.fn();
+    render(<Image onClick2={moveDropdownMenuMock} onClick1={moveTargetMock} />);
+    const image = screen.getByRole('img');
+
+    userEvent.click(image);
+    expect(moveDropdownMenuMock).toHaveBeenCalled();
+  });
+})
