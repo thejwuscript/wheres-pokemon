a4c0b737666008157b0fc872da6125ed119cfa2e
James Wu <ichangjwu@outlook.com>
2022-08-20 00:01:29 +0900

    Set state hasClicked

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useState } from "react";
 import Target from "./components/Target";
 import Image from "./components/Image";
 import DropdownMenu from "./components/DropdownMenu";
+import styled from "styled-components";
 
 function App() {
   const [visible, setVisible] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hasClicked, setHasClicked] = useState(false);
 
   const moveTargetAndMenu = (e) => {
+    if (hasClicked) {
+      setVisible(false);
+      setHasClicked(false);
+      return
+    }
+
     e.stopPropagation();
     setVisible(false);
     setTimeout(() => {
       setVisible(true);
       setPosition({ x: e.clientX, y: e.clientY });
     }, 0);
+    setHasClicked(true);
+  };
+
+  const removeTargetAndMenu = () => {
+    setVisible(false);
+    setHasClicked(false);
   };
 
   return (
-    <div>
+    <Wrapper onClick={removeTargetAndMenu}>
       <Image onClick={moveTargetAndMenu} />
       {visible && (
         <Target
@@ -26,8 +40,13 @@ function App() {
         />
       )}
       <DropdownMenu visible={visible} position={position} />
-    </div>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  display: flex;
+  width: min-content;
+  padding: 100px;
+`
 export default App;
