1b6ca126653ee58bd09e5b2f4f1f801796b6fa24
James Wu <ichangjwu@outlook.com>
2022-08-19 23:16:41 +0900

    Refactor Menu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,19 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function DropdownMenu({ visible, position }) {
+export default function DropdownMenu({ visible = false, position }) {
   const handleClick = (e) => e.stopPropagation();
 
   return (
     <Menu
       position={position}
+      visible={visible}
       role="menu"
       onClick={handleClick}
-      style={{
-        visibility: visible ? "visible" : "hidden",
-        height: visible ? 115 : 0,
-        transition: visible ? "height 0.4s ease" : "unset",
-      }}
     >
       <li>Pokemon 1</li>
       <hr />
@@ -26,8 +22,8 @@ export default function DropdownMenu({ visible, position }) {
 
 const Menu = styled.ul`
   width: 100px;
-  height: 0;
-  visibility: hidden;
+  height: ${(props) => (props.visible ? 115 : 0)}px;
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
   display: flex;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
   list-style: none;
@@ -41,7 +37,7 @@ const Menu = styled.ul`
   position: absolute;
   z-index: 2;
   background-color: white;
-  transition: height 0.4s ease;
+  transition: ${(props) => (props.visible ? "height 0.4s ease" : "unset")};
   overflow: hidden;
   border-radius: 4px;
 `;
