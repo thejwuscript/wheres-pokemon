bc82078250a041dd0431aec4435c2551b11177fd
James Wu <ichangjwu@outlook.com>
2022-08-20 17:13:07 +0900

    Add form

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -11,16 +11,24 @@ export default function DropdownMenu({ visible = false, position }) {
       role="menu"
       onClick={handleClick}
     >
-      <Item>Slowking</Item>
-      <hr />
-      <Item>Wobbuffet</Item>
-      <hr />
-      <Item>Pichu</Item>
+      <Form>
+        <Item name="name" value="slowking">
+          Slowking
+        </Item>
+        <hr />
+        <Item name="name" value="wobbuffet">
+          Wobbuffet
+        </Item>
+        <hr />
+        <Item name="name" value="pichu">
+          Pichu
+        </Item>
+      </Form>
     </Menu>
   );
 }
 
-const Menu = styled.ul`
+const Menu = styled.div`
   width: 100px;
   height: ${(props) => (props.visible ? 115 : 0)}px;
   visibility: ${(props) => (props.visible ? "visible" : "hidden")};
@@ -40,14 +48,26 @@ const Menu = styled.ul`
   border-radius: 4px;
 `;
 
-const Item = styled.li`
+const Item = styled.button`
   flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: white;
+  border: none;
   &:hover {
     background-color: black;
     color: white;
     cursor: pointer;
   }
+`;
+
+const Form = styled.form`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: center;
+  background-color: white;
+  border: none;
 `;
